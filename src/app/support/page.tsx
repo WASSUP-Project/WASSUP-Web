fd3ed929614e68b137925ec00a1469f02083928b
Yehyeok Bang <qkddpgur318@gmail.com>
2024-03-22 19:20:05 +0900

    [#14] feat: 고객지원 페이지 화면 구현
    - FAQ, 문의하기, 문의 내역 컴포넌트를 사용합니다.

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,8 +1,5 @@
-export default function Support() {
-  return (
-    <div>
-      <h1>Support</h1>
-      <p>This is the support page</p>
-    </div>
-  );
+import Support from "@/containers/support/Support";
+
+export default function SupportPage() {
+  return <Support />;
 }
---
+++ src/containers/support/Support.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Nav from "@/components/nav/Nav";
+import styles from "./Support.module.css";
+import FAQ from "@/containers/support/contents/FAQ";
+import Inquiry from "@/containers/support/contents/Inquiry";
+import MyInquiry from "@/containers/support/contents/MyInquiry";
+import { useState } from "react";
+
+export default function Support() {
+  const [activeMenu, setActiveMenu] = useState<string>("faq");
+
+  const handleMenuClick = (menu: string) => {
+    setActiveMenu(menu);
+  };
+
+  return (
+    <>
+      <div className={styles.supportContainer}>
+        <div>
+          <Nav text="로그인" positiveText="회원가입" />
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.leftNav}>
+            <h1 className={styles.navTitle}>고객 지원</h1>
+            <ul>
+              <li
+                className={activeMenu === "faq" ? styles.active : ""}
+                onClick={() => handleMenuClick("faq")}
+              >
+                <a href="#faq">자주 묻는 질문</a>
+              </li>
+              <li
+                className={activeMenu === "inquiry" ? styles.active : ""}
+                onClick={() => handleMenuClick("inquiry")}
+              >
+                <a href="#inquiry">문의하기</a>
+              </li>
+              <li
+                className={activeMenu === "my-inquiries" ? styles.active : ""}
+                onClick={() => handleMenuClick("my-inquiries")}
+              >
+                <a href="#my-inquiries">문의 내역</a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.rightContent}>
+            {activeMenu === "faq" && <FAQ />}
+            {activeMenu === "inquiry" && <Inquiry />}
+            {activeMenu === "my-inquiries" && <MyInquiry />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
