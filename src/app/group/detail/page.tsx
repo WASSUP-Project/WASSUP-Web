e5ff1a0784b383a1642a7290a921a74ced442aaf
Yehyeok Bang <qkddpgur318@gmail.com>
2024-03-24 23:49:47 +0900

    [#17] feat: 훅 사용 위치 관련 에러 수정
    - useSearchParams() Hook을 Suspense 경계 외부에서 사용하는 것을 수정합니다.

--- src/app/group/detail/page.tsx
+++ src/app/group/detail/page.tsx
@@ -1,6 +1,10 @@
-// url이 group/detail?id=1인 경우, id가 1인 그룹의 정보를 보여주는 페이지를 만드세요.
+import { Suspense } from "react";
 import GroupDetail from "@/containers/group/detail/GroupDetail";
 
 export default function GroupDetailPage() {
-  return <GroupDetail />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GroupDetail />
+    </Suspense>
+  );
 }
