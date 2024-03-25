1f4f3428572d364bebcbe0c70289923a98fc632d
Yehyeok Bang <qkddpgur318@gmail.com>
2024-03-25 14:04:27 +0900

    [#20] feat: 그룹 수정 페이지 화면 구현
    - 기능 구현은 API 완성 후 진행합니다.

--- src/types/group/group.ts
+++ src/types/group/group.ts
@@ -6,3 +6,12 @@ export type group = {
     waitMemberCount: number;
     groupImage: string;
 };
+
+export type groupInfo = {
+    id: number;
+    name: string;
+    address: string;
+    groupImage: string;
+    description: string;
+    businessNumber: string;
+};
