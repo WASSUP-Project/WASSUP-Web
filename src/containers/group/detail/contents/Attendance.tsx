11802d2a87900228cc5846ed01bf7e030c83d1c2
Yehyeok Bang <qkddpgur318@gmail.com>
2024-03-27 01:44:39 +0900

    [#20] feat: 출결 관리 페이지 화면 임시 구현
    - 임시 구현입니다.

--- src/containers/group/detail/contents/Attendance.tsx
+++ src/containers/group/detail/contents/Attendance.tsx
@@ -1,8 +1,33 @@
+import { Button } from "@nextui-org/react";
+import styles from "./Attendance.module.css";
+
 export default function Attendance() {
   return (
-    <div>
-      <h1>Attendance</h1>
-      <div>출석 관리 페이지입니다.</div>
-    </div>
+    <>
+      <h1 className={styles.title}>출결 관리</h1>
+      <div className={styles.container}>
+        <div className={styles.buttonContainer}>
+          <Button color="primary">출석 화면</Button>
+        </div>
+        <div className={styles.summary}>
+          <div className={styles.attendanceRate}>금일 출석률: 88% (22/25)</div>
+          <div className={styles.absent}>
+            <h3 className={styles.absentTitle}>금일 결석 인원</h3>
+            <ul className={styles.absentList}>
+              <li>김철수</li>
+              <li>홍길동</li>
+              <li>이영희</li>
+            </ul>
+          </div>
+          <div className={styles.late}>
+            <h3 className={styles.lateTitle}>금일 지각 인원</h3>
+            <ul className={styles.lateList}>
+              <li>박철수</li>
+              <li>김영희</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
