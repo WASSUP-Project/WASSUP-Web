bbd1149d8fe53bfed4e46a721ef3869dcc760e3f
Yehyeok Bang <qkddpgur318@gmail.com>
2024-04-04 20:57:03 +0900

    [#35] feat: 회원가입 기능 구현
    - 아이디 중복 확인, 인증번호 발송, 인증하기, 회원가입 기능을 추가합니다.

--- src/containers/signup/Signup.tsx
+++ src/containers/signup/Signup.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
 import styles from "./Signup.module.css";
 import Spacer from "@/components/Spacer";
 import Link from "next/link";
+import {
+  signup,
+  checkDuplicateId,
+  sendVerificationCode,
+  verifyCode,
+} from "@/services/signup/signup";
+import Image from "next/image";
 
 const validationSchema = yup.object().shape({
   id: yup
@@ -29,18 +36,6 @@ const validationSchema = yup.object().shape({
   verificationCode: yup.string().required("인증번호를 입력해주세요."),
 });
 
-const handleDuplicateCheck = () => {
-  console.log("handleDuplicateCheck");
-};
-
-const handleSendVerificationCode = () => {
-  console.log("handleSendVerificationCode");
-};
-
-const handleVerifyCode = () => {
-  console.log("handleVerifyCode");
-};
-
 export default function Signup() {
   const formik = useFormik({
     initialValues: {
@@ -56,6 +51,108 @@ export default function Signup() {
       console.log("Form submitted:", values);
     },
   });
+
+  const [isDuplicateId, setIsDuplicateId] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+  const [isVerified, setIsVerified] = useState(false);
+
+  const requestSignup = async () => {
+    if (!formik.isValid) {
+      return;
+    }
+
+    if (!isDuplicateId) {
+      alert("아이디 중복확인을 해주세요.");
+      return;
+    }
+
+    if (!isVerified) {
+      alert("휴대폰 인증을 해주세요.");
+      return;
+    }
+
+    const data = signup({
+      adminId: formik.values.id,
+      password: formik.values.password,
+      name: formik.values.name,
+      phoneNumber: formik.values.phoneNumber,
+    });
+
+    if (await data) {
+      alert("회원가입이 완료되었습니다.");
+      window.location.href = "/";
+    } else {
+      alert("회원가입에 실패했습니다. [관리자 문의]");
+    }
+  };
+
+  const requestCheckDuplicateId = async () => {
+    if (formik.values.id.length < 5) {
+      alert("아이디를 정확히 입력해주세요.");
+      return;
+    }
+
+    const data = checkDuplicateId(formik.values.id);
+
+    if ((await data) === false) {
+      setIsDuplicateId(true);
+      alert("사용 가능한 아이디입니다.");
+
+      const idInput = document.getElementById("id");
+      if (idInput) {
+        idInput.setAttribute("disabled", "true");
+      }
+
+      formik.setFieldValue("id", formik.values.id);
+    } else {
+      setIsDuplicateId(false);
+      alert("이미 사용 중인 아이디입니다.");
+    }
+  };
+
+  const requestSendVerificationCode = () => {
+    if (!formik.values.phoneNumber) {
+      alert("휴대폰 번호를 입력해주세요.");
+      return;
+    }
+
+    sendVerificationCode(formik.values.phoneNumber);
+    setIsSent(true);
+
+    const phoneNumberInput = document.getElementById("phoneNumber");
+    if (phoneNumberInput) {
+      phoneNumberInput.setAttribute("disabled", "true");
+    }
+
+    formik.setFieldValue("phoneNumber", formik.values.phoneNumber);
+  };
+
+  const requestVerifyCode = async () => {
+    if (!formik.values.verificationCode) {
+      alert("인증번호를 입력해주세요.");
+      return;
+    }
+
+    const data = verifyCode({
+      phoneNumber: formik.values.phoneNumber,
+      inputCertificationCode: formik.values.verificationCode,
+    });
+
+    if (await data) {
+      setIsVerified(true);
+      alert("인증되었습니다.");
+
+      const verificationCodeInput = document.getElementById("verificationCode");
+      if (verificationCodeInput) {
+        verificationCodeInput.setAttribute("disabled", "true");
+      }
+
+      formik.setFieldValue("verificationCode", formik.values.verificationCode);
+    } else {
+      setIsVerified(false);
+      alert("인증번호가 올바르지 않습니다.");
+    }
+  };
 
   return (
     <>
@@ -83,13 +180,23 @@ export default function Signup() {
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                 />
-                <button
-                  type="button"
-                  className={styles.button}
-                  onClick={() => handleDuplicateCheck()}
-                >
-                  중복확인
-                </button>
+                {isDuplicateId ? (
+                  <Image
+                    src="check.png"
+                    alt="check"
+                    width={20}
+                    height={20}
+                    className={styles.input_group_ok}
+                  />
+                ) : (
+                  <button
+                    type="button"
+                    className={styles.input_group_button}
+                    onClick={() => requestCheckDuplicateId()}
+                  >
+                    중복확인
+                  </button>
+                )}
               </div>
               {formik.touched.id && formik.errors.id && (
                 <div className={styles.errorMessage}>{formik.errors.id}</div>
@@ -157,13 +264,23 @@ export default function Signup() {
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                 />
-                <button
-                  type="button"
-                  className={styles.button}
-                  onClick={handleSendVerificationCode}
-                >
-                  발송하기
-                </button>
+                {isSent ? (
+                  <Image
+                    src="check.png"
+                    alt="check"
+                    width={20}
+                    height={20}
+                    className={styles.input_group_ok}
+                  />
+                ) : (
+                  <button
+                    type="button"
+                    className={styles.input_group_button}
+                    onClick={requestSendVerificationCode}
+                  >
+                    발송하기
+                  </button>
+                )}
               </div>
               {formik.touched.phoneNumber && formik.errors.phoneNumber && (
                 <div className={styles.errorMessage}>
@@ -183,13 +300,27 @@ export default function Signup() {
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                 />
-                <button
-                  type="button"
-                  className={styles.button}
-                  onClick={handleVerifyCode}
-                >
-                  인증하기
-                </button>
+                {isSent ? (
+                  isVerified ? (
+                    <Image
+                      src="check.png"
+                      alt="check"
+                      width={20}
+                      height={20}
+                      className={styles.input_group_ok}
+                    />
+                  ) : (
+                    <button
+                      type="button"
+                      className={styles.input_group_button}
+                      onClick={requestVerifyCode}
+                    >
+                      인증하기
+                    </button>
+                  )
+                ) : (
+                  <></>
+                )}
               </div>
               {formik.touched.verificationCode &&
                 formik.errors.verificationCode && (
@@ -204,7 +335,11 @@ export default function Signup() {
                   돌아가기
                 </button>
               </Link>
-              <button type="submit" className={styles.submitButton}>
+              <button
+                type="submit"
+                className={styles.submitButton}
+                onClick={requestSignup}
+              >
                 가입하기
               </button>
             </div>
