258c72b53f84e5790f4519f617394b427e6e5160
Yehyeok Bang <qkddpgur318@gmail.com>
2024-04-30 16:33:48 +0900

    [#59] feat: 출석 화면 구현
    - 4자리의 출석 번호를 입력할 수 있는 화면을 추가합니다.
    - 출석, 이미 출석, 중복된 멤버, 찾지 못함 상태가 존재합니다.

--- src/containers/group/detail/contents/AttendancePad.tsx
+++ src/containers/group/detail/contents/AttendancePad.tsx
@@ -2,10 +2,23 @@
 
 import React, { useState } from "react";
 import styles from "./AttendancePad.module.css";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Button,
+} from "@nextui-org/react";
+
+type AttendanceType = "ok" | "already" | "duplicate" | "notfound";
 
 export default function AttendancePad() {
   const [inputNumber, setInputNumber] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [attendanceType, setAttendanceType] = useState<AttendanceType>("ok");
 
   const handleNumberClick = (number: number) => {
     if (inputNumber.length < 4) {
@@ -18,23 +31,23 @@ export default function AttendancePad() {
   };
 
   const handleConfirmClick = () => {
-    if (inputNumber.length === 4) {
-      // TODO: API 연동
-      // 서버에 4자리 숫자 전송
-      // 서버에서 출석 정보 확인 후 결과 표시
-    } else {
+    if (inputNumber.length !== 4) {
       setErrorMessage("4자리 숫자를 입력하세요.");
+      return;
     }
+    setErrorMessage("");
+    setInputNumber("");
+    onOpen();
+  };
+
+  const requestAttendance = () => {
+    // TODO: 출석 요청
+    setAttendanceType("ok");
   };
 
   return (
-    <>
-      <header className={styles.header}>
-        <div className={styles.userInfo}>
-          <span>이름:</span>
-        </div>
-      </header>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <div className={styles.contents}>
         <div className={styles.inputNumber}>
           <input
             type="number"
@@ -47,15 +60,23 @@ export default function AttendancePad() {
           )}
         </div>
         <div className={styles.numberPad}>
-          {Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0).map((i) => (
+          {Array(1, 2, 3, 4, 5, 6, 7, 8, 9).map((i) => (
             <button
               key={i}
               className={styles.numberButton}
               onClick={() => handleNumberClick(i)}
+              onKeyDown={(e) => e.preventDefault()}
             >
               {i}
             </button>
           ))}
+          <div></div>
+          <button
+            className={styles.numberButton}
+            onClick={() => handleNumberClick(0)}
+          >
+            0
+          </button>
           <button
             className={styles.previousButton}
             onClick={handlePreviousClick}
@@ -67,6 +88,96 @@ export default function AttendancePad() {
           출석 확인
         </button>
       </div>
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) =>
+            attendanceType === "ok" ? (
+              OkModal(onClose)
+            ) : attendanceType === "already" ? (
+              AlreadyModal(onClose)
+            ) : attendanceType === "duplicate" ? (
+              DuplicateMemberModal(onClose)
+            ) : attendanceType === "notfound" ? (
+              NotFoundModal(onClose)
+            ) : (
+              <></>
+            )
+          }
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
+
+function OkModal(onClose: () => void) {
+  return (
+    <>
+      <ModalHeader className="flex flex-col gap-0">출석</ModalHeader>
+      <ModalBody className={styles.modal__body}>
+        <>
+          <div>출석되었습니다.</div>
+        </>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onPress={onClose}>
+          확인
+        </Button>
+      </ModalFooter>
+    </>
+  );
+}
+
+function AlreadyModal(onClose: () => void) {
+  return (
+    <>
+      <ModalHeader className="flex flex-col gap-0">출석</ModalHeader>
+      <ModalBody className={styles.modal__body}>
+        <>
+          <div>이미 출석하셨습니다.</div>
+        </>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onPress={onClose}>
+          확인
+        </Button>
+      </ModalFooter>
+    </>
+  );
+}
+
+function DuplicateMemberModal(onClose: () => void) {
+  return (
+    <>
+      <ModalHeader className="flex flex-col gap-0">선택하기</ModalHeader>
+      <ModalBody className={styles.modal__body}>
+        <>
+          <div>출석할 인원을 선택해주세요.</div>
+        </>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onPress={onClose}>
+          확인
+        </Button>
+      </ModalFooter>
+    </>
+  );
+}
+
+function NotFoundModal(onClose: () => void) {
+  return (
+    <>
+      <ModalHeader className="flex flex-col gap-0">출석</ModalHeader>
+      <ModalBody className={styles.modal__body}>
+        <>
+          <div>해당하는 인원이 없습니다.</div>
+        </>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onPress={onClose}>
+          확인
+        </Button>
+      </ModalFooter>
     </>
   );
 }
