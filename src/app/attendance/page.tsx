93ec8fc1843413e98d1d5db2cbe18c7e3dac9525
Yehyeok Bang <qkddpgur318@gmail.com>
2024-04-30 16:33:43 +0900

    fix: 임시 홈버튼 제거

--- src/app/attendance/page.tsx
+++ src/app/attendance/page.tsx
@@ -1,17 +1,5 @@
 import AttendancePad from "@/containers/group/detail/contents/AttendancePad";
-import { Button } from "@nextui-org/react";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function AttendancePage() {
-  return (
-    <>
-      <div>
-        <AttendancePad />
-        <Button color="primary">
-          <Link href="/">돌아가기</Link>
-        </Button>
-      </div>
-    </>
-  );
+  return <AttendancePad />;
 }
