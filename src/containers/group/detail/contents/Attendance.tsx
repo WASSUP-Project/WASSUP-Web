f62f534a40342607572afaf4f1e96974316b2fd4
Yehyeok Bang <qkddpgur318@gmail.com>
2024-05-04 00:08:13 +0900

    [#59] feat: 출석 내역 화면 및 링크 생성 화면 구현
    - 출석 페이지로 이동 가능한 링크를 생성합니다.
    - API 개발이 끝나면 마무리 작업 진행합니다.

--- src/containers/group/detail/contents/Attendance.tsx
+++ src/containers/group/detail/contents/Attendance.tsx
@@ -1,36 +1,121 @@
-import { Button } from "@nextui-org/react";
-import styles from "./Attendance.module.css";
-import Link from "next/link";
+"use client";
 
-export default function Attendance() {
+import styles from "./Attendance.module.css";
+import { useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Snippet,
+  Spinner,
+  Progress,
+  Button,
+} from "@nextui-org/react";
+import {
+  ResponseCode,
+  getAttendancePageUniqueCode,
+} from "@/services/attendance/attendance";
+
+type AttendanceProps = {
+  id: number;
+};
+
+export default function Attendance(props: AttendanceProps) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [attendanceCode, setAttendanceCode] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const generateAttendanceCode = async () => {
+    onOpen();
+    setIsLoading(true);
+    // const code = await getAttendanceCode();
+    const code = "Ri8DW19O"; // 임시 코드 API 개발 중
+    if (!code) {
+      alert("출석 링크를 생성하는데 실패했습니다. 잠시 후 다시 시도해주세요.");
+      return;
+    }
+    setAttendanceCode(code);
+    setIsLoading(false);
+  };
+
+  async function getAttendanceCode(): Promise<string> {
+    const response = (await getAttendancePageUniqueCode(
+      props.id
+    )) as ResponseCode;
+    return response.code;
+  }
+
+  function generateAttendancePageLink() {
+    return `${process.env.NEXT_PUBLIC_WEB_BASE_URL}/attendance?code=${attendanceCode}`;
+  }
+
   return (
     <>
       <h1 className={styles.title}>출결 관리</h1>
       <div className={styles.container}>
         <div className={styles.buttonContainer}>
-          <Button className={styles.button}>
-            <Link href="/attendance">출석</Link>
+          <Button className={styles.button} onClick={generateAttendanceCode}>
+            출석 링크
           </Button>
         </div>
         <div className={styles.summary}>
-          <div className={styles.attendanceRate}>금일 출석률: 88% (22/25)</div>
+          <Progress
+            label="금일 출석률"
+            aria-label="Downloading..."
+            size="md"
+            value={88}
+            color="primary"
+            showValueLabel={true}
+            className={styles.attendanceRate}
+          />
           <div className={styles.absent}>
-            <h3 className={styles.absentTitle}>금일 결석 인원</h3>
+            <h3 className={styles.absentTitle}>금일 미출석 인원</h3>
             <ul className={styles.absentList}>
               <li>김철수</li>
               <li>홍길동</li>
               <li>이영희</li>
             </ul>
           </div>
-          <div className={styles.late}>
-            <h3 className={styles.lateTitle}>금일 지각 인원</h3>
-            <ul className={styles.lateList}>
-              <li>박철수</li>
-              <li>김영희</li>
-            </ul>
-          </div>
         </div>
       </div>
+
+      <Modal
+        size="xl"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-0">
+                출석 페이지
+              </ModalHeader>
+              <ModalBody>
+                <>
+                  <div>아래 링크는 24시간 동안 유효합니다.</div>
+                  {isLoading ? (
+                    <Spinner size="lg" />
+                  ) : (
+                    <Snippet symbol="" variant="bordered">
+                      {generateAttendancePageLink()}
+                    </Snippet>
+                  )}
+                </>
+              </ModalBody>
+              <ModalFooter>
+                <Button className={styles.modalButton} onPress={onClose}>
+                  확인
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 }
