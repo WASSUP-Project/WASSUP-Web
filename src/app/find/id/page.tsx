8e7246aac387e814f3970ba1181943dd772701eb
Yehyeok Bang <qkddpgur318@gmail.com>
2024-05-21 21:50:21 +0900

    [#77] feat: 관리자 아이디 찾기 화면 임시 구현
    - API 구현이 완료되면 마무리할 예정입니다.

--- src/app/find/id/page.tsx
+++ src/app/find/id/page.tsx
@@ -1,8 +1,5 @@
-export default function FindId() {
-  return (
-    <div>
-      <h1>Find Id</h1>
-      <p>This is the find id page</p>
-    </div>
-  );
+import FindId from "@/containers/account/FindId";
+
+export default function FindIdPage() {
+  return <FindId />;
 }
