58a261f160e0cb10204441e3679e29e6602a8b62
Yehyeok Bang <qkddpgur318@gmail.com>
2024-05-23 22:23:17 +0900

    [#81] refactor: 고객 지원 화면 리팩토링
    - 채널톡 사용으로 사용하지 않는 화면을 비활성화 합니다.
    - 비즈니스 문의 화면을 추가합니다.

--- src/containers/support/contents/Inquiry.tsx
+++ src/containers/support/contents/Inquiry.tsx
@@ -1,76 +1,31 @@
 "use client";
 
-import React, { useState } from "react";
+import { Snippet } from "@nextui-org/react";
 import styles from "./Inquiry.module.css";
+import Spacer from "@/components/Spacer";
 
 export default function Inquiry() {
-  const [title, setTitle] = useState("");
-  const [email, setEmail] = useState("");
-  const [inquiry, setInquiry] = useState("");
-
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    alert(`
-      제목: ${title}
-      이메일: ${email}
-      문의 내용: ${inquiry}
-    `);
-  };
-
   return (
     <div className={styles.inquiryContainer}>
-      <h1 className={styles.inquiryTitle}>문의하기</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.inputGroup}>
-          <label htmlFor="title" className={styles.inquiryLabel}>
-            제목
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="제목을 입력해주세요."
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-            className={styles.inquiryInput}
-          />
-        </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="email" className={styles.inquiryLabel}>
-            이메일
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="결과 알림을 받을 이메일을 입력해주세요."
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className={styles.inquiryInput}
-          />
-        </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="inquiry" className={styles.inquiryLabel}>
-            문의 내용
-          </label>
-          <textarea
-            id="inquiry"
-            name="inquiry"
-            placeholder="문의 내용을 자세히 입력해주세요."
-            value={inquiry}
-            onChange={(e) => setInquiry(e.target.value)}
-            required
-            className={styles.inquiryTextArea}
-          />
-        </div>
-        <div className={styles.buttonGroup}>
-          <button type="submit" className={styles.inquiryButton}>
-            문의하기
-          </button>
-        </div>
-      </form>
+      <h1 className={styles.inquiryTitle}>비즈니스 문의하기</h1>
+      <br />
+      <p className={styles.subTitle}>✅ 도입에 대한 자세한 논의하기</p>
+      <br />
+      <p className={styles.subTitle}>✅ 제품 및 솔루션 가격에 대해 상담하기</p>
+      <br />
+      <p className={styles.subTitle}>✅ 업종별 사용 사례 살펴보기</p>
+      <Spacer height={3} />
+      <div>
+        <Snippet color="secondary" size="lg" symbol="">
+          skhu.wassup@gmail.com
+        </Snippet>
+        <Spacer height={3} />
+        <p className={styles.inquiryGuide}>
+          기업명, 연락처, 예상 인원 규모, 문의 내용 등 자세한 내용을 포함하여
+          <br />
+          문의해주시면 빠른 도움을 받아보실 수 있습니다.
+        </p>
+      </div>
     </div>
   );
 }
