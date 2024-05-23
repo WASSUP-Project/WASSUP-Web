bfd96e5e059e42860d5f24da4c842e5b4e1bb2b2
Yehyeok Bang <qkddpgur318@gmail.com>
2024-05-23 22:22:24 +0900

    [#81] feat: 채널톡 서비스 연동
    - 로그인 상태에 따라 채널톡 초기 메시지를 구분합니다.

--- src/containers/support/Support.tsx
+++ src/containers/support/Support.tsx
@@ -3,11 +3,36 @@
 import styles from "./Support.module.css";
 import FAQ from "@/containers/support/contents/FAQ";
 import Inquiry from "@/containers/support/contents/Inquiry";
-import MyInquiry from "@/containers/support/contents/MyInquiry";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ChannelService from "@/containers/support/services/ChannelService";
+import { useRecoilValue } from "recoil";
+import { adminState } from "@/states/admin";
 
 export default function Support() {
+  const admin = useRecoilValue(adminState);
   const [activeMenu, setActiveMenu] = useState<string>("faq");
+
+  useEffect(() => {
+    const channelTalk = new ChannelService();
+    if (admin == null) {
+      channelTalk.boot({
+        pluginKey: process.env.NEXT_PUBLIC_CHANNEL_IO_KEY,
+      });
+    } else {
+      const { id, name, phoneNumber } = admin;
+      channelTalk.boot({
+        pluginKey: process.env.NEXT_PUBLIC_CHANNEL_IO_KEY,
+        memberId: id,
+        profile: {
+          name,
+          mobileNumber: phoneNumber,
+        },
+      });
+    }
+    return () => {
+      channelTalk.shutdown();
+    };
+  }, [admin]);
 
   const handleMenuClick = (menu: string) => {
     setActiveMenu(menu);
@@ -29,6 +54,13 @@ export default function Support() {
               className={activeMenu === "inquiry" ? styles.active : ""}
               onClick={() => handleMenuClick("inquiry")}
             >
+              <a href="#inquiry">비즈니스 문의</a>
+            </li>
+            {/* 채널톡으로 변경함 */}
+            {/* <li
+              className={activeMenu === "inquiry" ? styles.active : ""}
+              onClick={() => handleMenuClick("inquiry")}
+            >
               <a href="#inquiry">문의하기</a>
             </li>
             <li
@@ -36,13 +68,12 @@ export default function Support() {
               onClick={() => handleMenuClick("my-inquiries")}
             >
               <a href="#my-inquiries">문의 내역</a>
-            </li>
+            </li> */}
           </ul>
         </div>
         <div className={styles.rightContent}>
           {activeMenu === "faq" && <FAQ />}
           {activeMenu === "inquiry" && <Inquiry />}
-          {activeMenu === "my-inquiries" && <MyInquiry />}
         </div>
       </div>
     </div>
